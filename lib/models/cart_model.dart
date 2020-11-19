b9eff5364a23b40cf2621ba6424eb24554bf0cd8
Clodoaldo Ribeiro <clodoaldorib@hotmail.com>
2020-11-19 15:53:01 -0300

    Update_cart_model
    Adicionar rotina de adicionar e remover itens do carinho de compras.

--- lib/models/cart_model.dart
+++ lib/models/cart_model.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:lojavirtual_app/datas/cart_product.dart';
 import 'package:lojavirtual_app/models/user_model.dart';
 import 'package:scoped_model/scoped_model.dart';
@@ -8,4 +9,32 @@ class CartModel extends Model {
   List<CartProduct> products = [];
 
   CartModel(this.user) {}
+
+  void addCartItem(CartProduct cartProduct) {
+    products.add(cartProduct);
+
+    Firestore.instance
+        .collection("users")
+        .document(user.firebaseUser.uid)
+        .collection("cart")
+        .add(cartProduct.toMap())
+        .then((doc) {
+      cartProduct.cid = doc.documentID;
+    });
+
+    notifyListeners();
+  }
+
+  void removeCartItem(CartProduct cartProduct) {
+    Firestore.instance
+        .collection("users")
+        .document(user.firebaseUser.uid)
+        .collection("cart")
+        .document(cartProduct.cid)
+        .delete();
+
+    products.remove(cartProduct);
+
+    notifyListeners();
+  }
 }
